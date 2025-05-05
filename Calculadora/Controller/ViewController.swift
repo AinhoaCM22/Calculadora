97440e196f1c8911c48ba6e57c1e09b9959c949c
Ainhoa Casas Montero <ainhoa.casasmontero@plexus.es>
2025-05-05 08:24:40 +0200

    Optimizado

--- Calculadora/Controller/ViewController.swift
+++ Calculadora/Controller/ViewController.swift
@@ -9,148 +9,97 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var resultado: UILabel!
+    @IBOutlet weak var result: UILabel!
     var model : Model = Model()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        resultado.text = "0"
+        result.text = "0"
     }
     
-   
-        
-        @IBAction func division(_ sender: Any) {
-            model.setOperacion("/" , resultado )
+        @IBAction func divide(_ sender: Any) {
+            model.setOperation("/" , result )
         }
         
-        @IBAction func multiplicacion(_ sender: Any) {
-            model.setOperacion("*" , resultado )
+        @IBAction func multiply(_ sender: Any) {
+            model.setOperation("*" , result )
         }
         
-        @IBAction func restar(_ sender: Any) {
-            model.setOperacion("-" , resultado )
+        @IBAction func subtract(_ sender: Any) {
+            model.setOperation("-" , result )
         }
         
-        @IBAction func sumar(_ sender: Any) {
-            model.setOperacion("+" , resultado )
+        @IBAction func add(_ sender: Any) {
+            model.setOperation("+" , result )
         }
         
     
-        @IBAction func igual(_ sender: Any) {
-            var result : Double
-            var primerNumero = Double(model.num1.replacingOccurrences(of: ",", with: ".")) ?? 0.0
-            var segundoNumero = Double(model.num2.replacingOccurrences(of: ",", with: ".")) ?? 0.0
-          
-            
-            switch (model.operacion) {
-            case "/" :
-                result = (primerNumero / segundoNumero)
-                resultado.text = Model.setFormat(result)
-                model.num1 = Model.setFormat(result)
-                model.num2 = ""
-                model.operacion = ""
-                break
-            case "*" :
-                result = primerNumero * segundoNumero
-                resultado.text = Model.setFormat(result)
-                model.num1 = Model.setFormat(result)
-                model.num2 = ""
-                model.operacion = ""
-                break
-            case "+" :
-                result = (primerNumero + segundoNumero)
-                resultado.text = Model.setFormat(result)
-                model.num1 = Model.setFormat(result)
-                model.num2 = ""
-                model.operacion = ""
-                break
-            case "-" :
-                result = primerNumero - segundoNumero
-                resultado.text = Model.setFormat(result)
-                model.num1 = Model.setFormat(result)
-                model.num2 = ""
-                model.operacion = ""
-                break
-            case "%" :
-                result = primerNumero * 0.01
-                resultado.text = Model.setFormat(result)
-                model.num1 = Model.setFormat(result)
-                model.num2 = ""
-                model.operacion = ""
-                break
-            default :
-                break
-            }
-            model.cambiarNum = false
+        @IBAction func equal(_ sender: Any) {
+            result.text = model.equal()
         }
         
-        @IBAction func porcentaje(_ sender: Any) {
-            model.setOperacion("%" , resultado)
+        @IBAction func percentage(_ sender: Any) {
+            model.setOperation("%" , result)
         }
         
-        @IBAction func cambiarSigno(_ sender: Any) {
-            if(model.signo){
-                model.signo = false
+        @IBAction func changesign(_ sender: Any) {
+            if(model.sign){
+                model.sign = false
             } else {
-                model.signo = true
+                model.sign = true
             }
-            model.comprobarSigno(model.signo, model.cambiarNum , resultado)
-        }
-        
-      
-        
-    @IBAction func borrar(_ sender: Any) {
-        model.num1 = ""
-        model.num2 = ""
-        model.operacion = ""
-        resultado.text = "0"
-        model.cambiarNum = false
+            model.checkSign(model.sign, model.changeNum , result)
         }
     
-        @IBAction func addNueve(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "9")
+    @IBAction func erase(_ sender: Any) {
+        model.erase()
+        result.text = "0"
+        }
+    
+        @IBAction func addNine(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "9")
         }
         
         
-        @IBAction func addOcho(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "8")
+        @IBAction func addEight(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "8")
         }
         
-        @IBAction func addSiete(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "7")
+        @IBAction func addSeven(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "7")
         }
         
-        @IBAction func addSeis(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "6")
+        @IBAction func addSix(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "6")
         }
         
-        @IBAction func addCinco(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "5")
+        @IBAction func addFive(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "5")
         }
         
-        @IBAction func addCuatro(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "4")
+        @IBAction func addFour(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "4")
         }
         
-        @IBAction func addTres(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "3")
+        @IBAction func addThree(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "3")
         }
         
-        @IBAction func addDos(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "2")
-        }
-        
-        
-        @IBAction func addUno(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "1")
+        @IBAction func addTwo(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "2")
         }
         
         
-        @IBAction func addCero(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, "0")
+        @IBAction func addOne(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "1")
+        }
+        
+        
+        @IBAction func addZero(_ sender: Any) {
+            result.text = model.setnum(model.changeNum, "0")
         }
         
         @IBAction func addComa(_ sender: Any) {
-            resultado.text = model.setnum(model.cambiarNum, ",")
+            result.text = model.setnum(model.changeNum, ",")
         }
 }
--- Calculadora/Model/ViewModel.swift
+++ Calculadora/Model/ViewModel.swift
@@ -12,25 +12,34 @@ class Model {
     
     var num1 = "0"
     var num2 = "0"
-    var operacion = ""
-    var cambiarNum = false
-    var signo = true
+    var operation = ""
+    var changeNum = false
+    var sign = true
+    var resultOperation : Double = 0.0
     
     func Model () {
         num1 = "0"
         num2 = "0"
-        operacion = ""
-        cambiarNum = false
-        signo = true
+        operation = ""
+        changeNum = false
+        sign = true
     }
     
-    func setOperacion (_ texto : String , _ resultado : UILabel) {
-        operacion = texto
-        resultado.text = "0"
-        cambiarNum = true
+    func erase () {
+        num1 = "0"
+        num2 = "0"
+        operation = ""
+        changeNum = false
+        sign = true
     }
-
-    static func setFormat (_ result : Double) -> String {
+    
+    func setOperation (_ text : String , _ result : UILabel) {
+        operation = text
+        result.text = "0"
+        changeNum = true
+    }
+    
+    func setFormat (_ result : Double) -> String {
         if(result.truncatingRemainder(dividingBy: 1) == 0 ) {
             return String(format: "%.0f", result)
         }
@@ -38,47 +47,90 @@ class Model {
             return String(format: "%.5f", result)
         }
     }
-
-    func comprobarSigno(_ signo : Bool , _ cambiarNum : Bool , _ resultado : UILabel) {
-        if (signo == false) {
-            if (!cambiarNum) {
+    
+    func checkSign(_ sign : Bool , _ changeNum : Bool , _ result : UILabel) {
+        if (sign == false) {
+            if (!changeNum) {
                 num1 = "-" + num1
-                resultado.text = num1
+                result.text = num1
             } else {
                 num2 = "-" + num2
-                resultado.text = num2
+                result.text = num2
             }
         } else {
-            if (!cambiarNum) {
+            if (!changeNum) {
                 num1 = num1.replacingOccurrences(of: "-", with: "")
-                resultado.text = num1
+                result.text = num1
             } else {
                 num2 = num2.replacingOccurrences(of: "-", with: "")
-                resultado.text = num2
+                result.text = num2
             }
         }
     }
-        
+    
     func setnum (_ cambiarNum : Bool , _ num : String) -> String {
-            if(!cambiarNum) {
-                if (num1 == "0"){
-                    num1 = num
-                    return num1
-                } else {
-                    num1 = num1 + num
-                    return num1
-                }
+        if(!cambiarNum) {
+            if (num1 == "0"){
+                num1 = num
+                return num1
             } else {
-                if (num2 == "0"){
-                    num2 = num
-                    return num2
-                } else {
-                    num2 = num2 + num
-                    return num2
-                }
+                num1 = num1 + num
+                return num1
             }
-            
+        } else {
+            if (num2 == "0"){
+                num2 = num
+                return num2
+            } else {
+                num2 = num2 + num
+                return num2
+            }
         }
-}
+        
+    }
+    
+    func equal () -> String {
+        var firstNumber = Double(num1.replacingOccurrences(of: ",", with: ".")) ?? 0.0
+        var secondNumber = Double(num2.replacingOccurrences(of: ",", with: ".")) ?? 0.0
+        
+        
+        switch (operation) {
+        case "/" :
+            resultOperation = (firstNumber / secondNumber)
+            num1 = setFormat(resultOperation)
+            num2 = ""
+            operation = ""
+            break
+        case "*" :
+            resultOperation = firstNumber * secondNumber
+            num1 = setFormat(resultOperation)
+            num2 = ""
+            operation = ""
+            break
+        case "+" :
+            resultOperation = (firstNumber + secondNumber)
+            num1 = setFormat(resultOperation)
+            num2 = ""
+            operation = ""
+            break
+        case "-" :
+            resultOperation = firstNumber - secondNumber
+            num1 = setFormat(resultOperation)
+            num2 = ""
+            operation = ""
+            break
+        case "%" :
+            resultOperation = firstNumber * 0.01
+            num1 = setFormat(resultOperation)
+            num2 = ""
+            operation = ""
+            break
+        default :
+            break
+        }
+            changeNum = false
+            return setFormat(resultOperation)
+        }
+    }
 
 
